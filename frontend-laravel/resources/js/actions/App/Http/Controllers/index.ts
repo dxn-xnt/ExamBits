b75bdd7bc333307bd230ff049536cf9753ae65a7
Dan Anton Bejec <dxn.xnt@gmail.com>
2025-11-25 15:45:51 +0800

    Initial Commit for Laravel Front End

---
+++ frontend-laravel/resources/js/actions/App/Http/Controllers/index.ts
@@ -0,0 +1,8 @@
+import ExamController from './ExamController'
+
+
+const Controllers = {
+    ExamController: Object.assign(ExamController, ExamController),
+}
+
+export default Controllers
---
+++ frontend-laravel/resources/js/components/ui/tabs.tsx
@@ -0,0 +1,64 @@
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+
+import { cn } from "@/lib/utils"
+
+function Tabs({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn("flex flex-col ", className)}
+      {...props}
+    />
+  )
+}
+
+function TabsList({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+          "flex text-muted-foreground gap-4 h-fit w-full px-3 border-b border-border overflow-auto",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TabsTrigger({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        "inline-flex h-9 items-center justify-center gap-1.5 px-1 py-1 text-sm font-medium rounded-none text-foreground dark:text-foreground data-[state=active]:text-primary dark:data-[state=active]:text-primary transition-colors focus-visible:ring-[3px] focus-visible:outline-1 data-[state=active]:border-b data-[state=active]:border-primary dark:data-[state=active]:border-primary disabled:pointer-events-none disabled:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TabsContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn("flex-1 w-full overflow-auto", className)}
+      {...props}
+    />
+  )
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
